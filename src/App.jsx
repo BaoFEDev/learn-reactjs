3f2e11779f2adea31b31acca310941ebb9891610
Nguyen Hoan Bao <baohoannguyenblog01@gmail.com>
2021-02-23 01:02:54 +0700

    learn-reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ function App() {
   let profile = {
     name: "Nguyễn Hoàn Bảo",
     job: "Dev",
+    age: 24,
   };
-  let { name, job } = profile;
+  let { name, job, age } = profile;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{name}</p>
         <p>{job}</p>
+        <p>{age}</p>
       </header>
     </div>
   );
