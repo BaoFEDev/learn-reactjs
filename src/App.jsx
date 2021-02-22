2ad91478c17993cfe6626b4621c62ac68c4aef68
Nguyen Hoan Bao <baohoannguyenblog01@gmail.com>
2021-02-23 00:57:08 +0700

    learn-reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let profile = {
+    name: "Nguyễn Hoàn Bảo",
+    job: "Dev",
+  };
+  let { name, job } = profile;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hoàn Bảo
-        </p>
+        <p>{name}</p>
+        <p>{job}</p>
       </header>
     </div>
   );
